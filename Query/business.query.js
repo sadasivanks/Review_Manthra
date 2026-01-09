eeaadecd2ffa9855d473060f0a147d66d1a25470
sadasivanks <79564051+sadasivanks@users.noreply.github.com>
2026-01-09 11:34:33 +0530

    model name updated

--- Query/business.query.js
+++ Query/business.query.js
@@ -4,7 +4,7 @@ import { supabase } from "../src/supabaseClient.js";
 
 export const getBusinessPlansQuery = async () => {
     const { data, error } = await supabase
-        .from("tb_business_modals")
+        .from("tb_business_models")
         .select("*");
 
     if (error) {
